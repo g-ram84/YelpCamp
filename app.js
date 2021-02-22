de99e081643dff2ab4bef1f49c3bbc70f83a8238
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-02-21 18:12:28 -0800

    Added authentication

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user')
 
-
-const reviews = require('./routes/reviews')
-const campgrounds = require('./routes/campgrounds');
+const userRoutes = require('./routes/users')
+const reviewRoutes = require('./routes/reviews')
+const campgroundRoutes = require('./routes/campgrounds');
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
 	useNewUrlParser: true,
@@ -48,14 +51,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
 	res.locals.success = req.flash('success');
 	res.locals.error = req.flash('error')
 	next();
 })
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 app.get("/", (req, res) => {
 	res.render("home");
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require("../models/campground");
 const { campgroundSchema } = require('../schemas.js');
+const { isLoggedIn } = require('../middleware')
 
 const validateCampground = (req, res, next) => {
   const { error } = campgroundSchema.validate(req.body);
@@ -20,11 +21,11 @@ router.get('/', catchAsync(async (req, res, next) => {
   res.render('campgrounds/index', { campgrounds })
 }));
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
   res.render('campgrounds/new')
 });
 
-router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
   const campground = new Campground(req.body.campground);
   await campground.save();
   req.flash('success', 'Successfully made a new campground!')
@@ -32,15 +33,17 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 }));
 
 router.get('/:id', catchAsync(async (req, res, next) => {
-  const campground = await Campground.findById(req.params.id).populate('reviews');
+  const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+  console.log(campground)
   if (!campground) {
     req.flash('error', 'Cannot find that campground!')
     res.redirect('/campgrounds')
+  } else {
+    res.render('campgrounds/show', { campground });
   }
-  res.render('campgrounds/show', { campground });
 }));
 
-router.get('/:id/edit', catchAsync(async (req, res, next) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res, next) => {
   const campground = await Campground.findById(req.params.id)
   if (!campground) {
     req.flash('error', 'Cannot find that campground!')
@@ -50,14 +53,14 @@ router.get('/:id/edit', catchAsync(async (req, res, next) => {
   }
 }));
 
-router.put('/:id', validateCampground, catchAsync(async (req, res, next) => {
+router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
   req.flash('success', 'Successfully updated campground')
   res.redirect(`/campgrounds/${campground._id}`)
 }));
 
-router.delete('/:id', catchAsync(async (req, res, next) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res, next) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
   req.flash('success', 'Successfully deleted campground!')
