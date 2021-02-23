b0a49e0668e9d306373800d854344d264728cb41
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-02-22 18:23:34 -0800

    Sanitize HTML

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const User = require('./models/user')
+const User = require('./models/user');
+
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users')
 const reviewRoutes = require('./routes/reviews')
@@ -39,6 +41,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 
 const sessionConfig = {
