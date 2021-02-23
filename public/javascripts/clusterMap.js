30676da2fd8e3587c7e881c80f5b56f53f3b812c
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-02-22 16:26:51 -0800

    Added popups to cluster map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,5 +1,5 @@
 mapboxgl.accessToken = mapToken
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/dark-v10',
 center: [-103.59179687498357, 40.66995747013945],
@@ -35,20 +35,20 @@ paint: {
 'circle-color': [
 'step',
 ['get', 'point_count'],
-'#51bbd6',
-100,
-'#f1f075',
-750,
-'#f28cb1'
+'#00BCD4',
+10,
+'#2196F3',
+30,
+'#3F51B5'
 ],
 'circle-radius': [
 'step',
 ['get', 'point_count'],
+15,
+10,
 20,
-100,
 30,
-750,
-40
+25
 ]
 }
 });
@@ -73,17 +73,17 @@ filter: ['!', ['has', 'point_count']],
 paint: {
 'circle-color': '#11b4da',
 'circle-radius': 20,
-'circle-stroke-width': 10,
+'circle-stroke-width': 2,
 'circle-stroke-color': '#fff'
 }
 });
  
 // inspect a cluster on click
 map.on('click', 'clusters', function (e) {
-var features = map.queryRenderedFeatures(e.point, {
+const features = map.queryRenderedFeatures(e.point, {
 layers: ['clusters']
 });
-var clusterId = features[0].properties.cluster_id;
+const clusterId = features[0].properties.cluster_id;
 map.getSource('campgrounds').getClusterExpansionZoom(
 clusterId,
 function (err, zoom) {
@@ -102,16 +102,9 @@ zoom: zoom
 // the location of the feature, with
 // description HTML from its properties.
 map.on('click', 'unclustered-point', function (e) {
-var coordinates = e.features[0].geometry.coordinates.slice();
-var mag = e.features[0].properties.mag;
-var tsunami;
- 
-if (e.features[0].properties.tsunami === 1) {
-tsunami = 'yes';
-} else {
-tsunami = 'no';
-}
- 
+  const {popUpMarkup} = e.features[0].properties
+const coordinates = e.features[0].geometry.coordinates.slice();
+
 // Ensure that if the map is zoomed out such that
 // multiple copies of the feature are visible, the
 // popup appears over the copy being pointed to.
@@ -121,9 +114,7 @@ coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
  
 new mapboxgl.Popup()
 .setLngLat(coordinates)
-.setHTML(
-'magnitude: ' + mag + '<br>Was there a tsunami?: ' + tsunami
-)
+.setHTML(popUpMarkup)
 .addTo(map);
 });
  
