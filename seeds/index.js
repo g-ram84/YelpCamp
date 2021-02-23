176f5c57fe74be82431572c70cbfaf4e018e7565
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-02-22 16:02:02 -0800

    Set up cluster map

--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
 	await Campground.deleteMany({})
-	for (let i = 0; i < 50; i++) {
+	for (let i = 0; i < 300; i++) {
 		const random1000 = Math.floor(Math.random() * 1000)
 		const price = Math.floor(Math.random() * 20) + 10;
 		const camp = new Campground({
@@ -48,8 +48,14 @@ const seedDB = async () => {
 			}
 			],
 			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum enim lorem, mattis et nisi quis, facilisis tempus leo. Cras sed.',
-			price
-
+			price,
+			geometry: {
+				type: 'Point',
+				coordinates: [
+					cities[random1000].longitude,
+					cities[random1000].latitude
+				]
+			},
 		})
 		await camp.save();
 	}
